b3efe2e7c0b8d0dfdea5d0128a8744a61dd199e9
carollwd <sreemithra1618@gmail.com>
2026-08-14 15:41:30 +0000

    Save progress

---
+++ week_7/main.py
@@ -0,0 +1,21 @@
+from food_order import calculate_total
+
+
+def main():
+    try:
+        price = float(input("Price (RM): "))
+        quantity = int(input("Quantity: "))
+
+        total = calculate_total(price, quantity)
+
+        if isinstance(total, (int, float)):
+            print(f"Total Payment = RM {total:.2f}")
+        else:
+            print(f"Error: {total}")
+
+    except ValueError:
+        print("Invalid input! Please enter numerical values.")
+
+
+if __name__ == "__main__":
+    main()
